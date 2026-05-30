7a8d4a06b6f8fa4cb65313611a54267f3b30616e
sp <68207888+SampleProvider@users.noreply.github.com>
2026-05-30 14:40:14 -0400

    fix server ip

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ window.addEventListener("resize", () => {
 });
 onResize();
 
-const serverIp = new URLSearchParams(window.location.search).get("ip") ?? "https://battlepixelsserver.onrender.com/";
+// const serverIp = new URLSearchParams(window.location.search).get("ip") ?? "https://battlepixelsserver.onrender.com/";
+const serverIp = new URLSearchParams(window.location.search).get("ip") ?? "https://gulizi.dyndns.org:10003/";
 
 const socket = io(serverIp, {
     autoConnect: false,
@@ -17,6 +18,21 @@ const emitFn = socket.emit;
 // @ts-ignore
 // socket.emit = (...args) => setTimeout(() => {
 //     emitFn.apply(socket, args);
-// }, 1000); 
+// }, 1000);
+
+async function checkConnection() {
+    try {
+        await fetch(serverIp);
+    }
+    catch (err) {
+        if (navigator.onLine && err instanceof TypeError && err.message == "Failed to fetch") {
+            document.getElementById("canvas").style.display = "none";
+            document.getElementById("webgpuCanvas").style.display = "none";
+            document.getElementById("menuScreen").style.display = "none";
+            document.getElementById("connectionError").style.display = "revert-layer";
+        }
+    }
+};
+checkConnection();
 
 export { serverIp, socket };
